717dce5f0219635b1c1f5bd16c23b4d85e6b3bb3
Sam Ward <43907088+swardeo@users.noreply.github.com>
2021-03-18 16:56:16 +0000

    Implement plotting of debts (#23)

--- src/components/Graph/Graph.stories.js
+++ src/components/Graph/Graph.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Graph from './index';
-import plotSavings from '../../data/plotSavings';
+import plot from '../../data';
 
 export default {
     title: 'Components/Graph',
@@ -9,37 +9,67 @@ export default {
 
 const savings = [
     {
-        name: 'Pension 1',
-        startAmount: '100.00',
+        name: 'House Deposit',
+        startAmount: '0.00',
         monthlyAmount: '100.00',
         startDate: '2015-01',
-        endDate: '2055-12',
-        yearlyRate: '6.00',
+        endDate: '2020-10',
+        yearlyRate: '0.30',
         adjustments: [
             {
-                amount: '0.00',
-                dateFrom: '2025-01',
-                rate: '6.00',
+                amount: '75.00',
+                dateFrom: '2017-01',
+                rate: '0.40',
             },
         ],
+        oneTimePayments: [],
     },
     {
-        name: 'Pension 2',
+        name: 'Wedding',
         startAmount: '0.00',
-        monthlyAmount: '0.00',
-        startDate: '2015-01',
-        endDate: '2055-12',
-        yearlyRate: '0.00',
-        adjustments: [
+        monthlyAmount: '150.00',
+        startDate: '2014-05',
+        endDate: '2017-07',
+        yearlyRate: '0.20',
+        adjustments: [],
+        oneTimePayments: [
             {
-                amount: '100.00',
-                dateFrom: '2025-01',
-                rate: '6.00',
+                amount: '400.00',
+                date: '2015-11',
             },
         ],
     },
 ];
 
-const data = plotSavings(savings);
+const debts = [
+    {
+        name: 'Car Loan',
+        startAmount: '5000.00',
+        monthlyAmount: '200.00',
+        startDate: '2015-02',
+        yearlyRate: '3.00',
+        adjustments: [],
+        oneTimePayments: [],
+    },
+    {
+        name: 'Holiday',
+        startAmount: '2000.00',
+        monthlyAmount: '100.00',
+        startDate: '2011-11',
+        yearlyRate: '3.00',
+        adjustments: [
+            {
+                amount: '500.00',
+                dateFrom: '2012-02',
+                rate: '1.50',
+            },
+        ],
+        oneTimePayments: [],
+    },
+];
 
-export const BasicGraph = () => <Graph data={data} />;
+export const SavingsOnly = () => <Graph data={plot(savings, [])} />;
+
+export const DebtsOnly = () => <Graph data={plot([], debts)} />;
+
+export const Both = () => <Graph data={plot(savings, debts)} />;
--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,143 @@
-import plotSavings from './plotSavings';
+import { BigNumber } from 'bignumber.js';
+import { DateTime } from 'luxon';
 
-const plot = (savings) => {
-    return plotSavings(savings);
+const formatDate = (date) => {
+    return `${date.toFormat('y')}-${date.toFormat('MM')}`;
+};
+
+const formatNumber = (number) => {
+    return number.toFixed(2);
+};
+
+const addMonth = (date) => {
+    return date.plus({ months: 1 });
+};
+
+const calculateMonthlyInterest = (amount, rate) => {
+    const yearly = rate.div(100);
+    const monthly = yearly.div(12);
+    const interest = amount.multipliedBy(monthly);
+    return interest;
+};
+
+const orderedOneTimePayments = (oneTimePayments) => {
+    const mappedOneTimePayments = oneTimePayments.map((oneTimePayment) => ({
+        date: DateTime.fromISO(oneTimePayment.date),
+        amount: new BigNumber(oneTimePayment.amount),
+    }));
+    return mappedOneTimePayments.sort(
+        (a, b) => a.date.toMillis() - b.date.toMillis()
+    );
+};
+
+const orderedAdjustments = (adjustments) => {
+    const mappedAdjustments = adjustments.map((adjustment) => ({
+        amount: new BigNumber(adjustment.amount),
+        dateFrom: DateTime.fromISO(adjustment.dateFrom),
+        rate: new BigNumber(adjustment.rate),
+    }));
+    return mappedAdjustments.sort(
+        (a, b) => a.dateFrom.toMillis() - b.dateFrom.toMillis()
+    );
+};
+
+const Type = {
+    SAVING: 'saving',
+    DEBT: 'debt',
+};
+
+const calculate = (inputs, type) => {
+    if (inputs.length === 0) {
+        return [];
+    }
+
+    let plotted = [];
+
+    inputs.forEach((input) => {
+        let plottedData = [];
+
+        let currentAmount = new BigNumber(input.startAmount);
+        let currentDate = DateTime.fromISO(input.startDate);
+        let currentMonthlyAmount = new BigNumber(input.monthlyAmount);
+        let currentRate = new BigNumber(input.yearlyRate);
+        const endDate =
+            type === Type.SAVING
+                ? DateTime.fromISO(input.endDate)
+                : DateTime.fromISO('2050-12');
+
+        let adjustments =
+            input?.adjustments?.length > 0
+                ? orderedAdjustments(input.adjustments)
+                : [];
+        let oneTimePayments =
+            input?.oneTimePayments?.length > 0
+                ? orderedOneTimePayments(input.oneTimePayments)
+                : [];
+
+        plottedData.push({
+            x: formatDate(currentDate),
+            y: formatNumber(currentAmount),
+        });
+        currentDate = addMonth(currentDate);
+
+        while (
+            currentDate <= endDate &&
+            (type === Type.DEBT ? currentAmount.gt(BigNumber(0)) : true)
+        ) {
+            currentAmount = currentAmount.plus(
+                calculateMonthlyInterest(currentAmount, currentRate)
+            );
+
+            if (
+                adjustments.length > 0 &&
+                currentDate.hasSame(adjustments[0].dateFrom, 'day')
+            ) {
+                const adjustment = adjustments.shift();
+                currentMonthlyAmount = adjustment.amount;
+                currentRate = adjustment.rate;
+            }
+
+            currentAmount =
+                type === Type.SAVING
+                    ? currentAmount.plus(currentMonthlyAmount)
+                    : currentAmount.minus(currentMonthlyAmount);
+
+            if (
+                oneTimePayments.length > 0 &&
+                currentDate.hasSame(oneTimePayments[0].date, 'day')
+            ) {
+                const oneTimeAmount = oneTimePayments.shift().amount;
+                currentAmount =
+                    type === Type.SAVING
+                        ? currentAmount.plus(oneTimeAmount)
+                        : currentAmount.minus(oneTimeAmount);
+            }
+
+            if (type === Type.DEBT) {
+                currentAmount = currentAmount.gt(BigNumber(0))
+                    ? currentAmount
+                    : BigNumber(0);
+            }
+
+            plottedData.push({
+                x: formatDate(currentDate),
+                y: formatNumber(currentAmount),
+            });
+
+            currentDate = addMonth(currentDate);
+        }
+
+        plotted.push({
+            id: `(${type === Type.SAVING ? 'S' : 'D'}) ${input.name}`,
+            data: plottedData,
+        });
+    });
+
+    return plotted;
+};
+
+const plot = (savings, debts) => {
+    return calculate(savings, Type.SAVING).concat(calculate(debts, Type.DEBT));
 };
 
 export default plot;
